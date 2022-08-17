7dbe5f12b19fb9f3b3ae1ab12c1269c55c674690
Fgsilva <fgsilva@ice.ufjf.br>
2022-08-16 21:23:27 -0300

    config variation to change images and make bird fly

--- core/src/com/lab/flappybird/Jogo.java
+++ core/src/com/lab/flappybird/Jogo.java
@@ -10,16 +10,21 @@ public class Jogo extends ApplicationAdapter {
 	private int moveX = 0;
 	private int moveY = 0;
 	private SpriteBatch batch;
-	private Texture bird;
+	private Texture[] birds;
 	private Texture background;
 
 	private float width;
 	private float height;
+	private float variation = 0;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		bird = new Texture("passaro1.png");
+		birds = new Texture[3];
+		birds[0] = new Texture("passaro1.png");
+		birds[1] = new Texture("passaro2.png");
+		birds[2] = new Texture("passaro3.png");
+
 		background = new Texture("fundo.png");
 
 		width = Gdx.graphics.getWidth();
@@ -30,9 +35,13 @@ public class Jogo extends ApplicationAdapter {
 	public void render () {
 		batch.begin();
 
-		batch.draw(background, 0, 0, width, height);
-		batch.draw(bird, moveX, moveY);
+		if(variation > 3)
+			variation = 0;
 
+		batch.draw(background, 0, 0, width, height);
+		batch.draw(birds[(int)  variation], 30, height/2);
+
+		variation += Gdx.graphics.getDeltaTime() * 10;
 		moveX++;
 		moveY++;
 		batch.end();
