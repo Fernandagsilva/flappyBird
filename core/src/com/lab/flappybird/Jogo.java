180cc68717f763ade655174e7f7032ca490c1f7a
Fgsilva <fgsilva@ice.ufjf.br>
2022-08-16 22:43:09 -0300

    add pipes to game

--- core/src/com/lab/flappybird/Jogo.java
+++ core/src/com/lab/flappybird/Jogo.java
@@ -4,43 +4,47 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Jogo extends ApplicationAdapter {
-	private int moveX = 0;
-	private int moveY = 0;
 	private SpriteBatch batch;
 	private Texture[] birds;
 	private Texture background;
+	private Texture pipeBottom;
+	private Texture pipeTop;
 
 	private float width;
 	private float height;
 	private float variation = 0;
 	private  float gravity = 0;
 	private float startPositionY = 0;
+	private float pipeWidthPosition;
+	private float pipesSpaceBetween;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		birds = new Texture[3];
-		birds[0] = new Texture("passaro1.png");
-		birds[1] = new Texture("passaro2.png");
-		birds[2] = new Texture("passaro3.png");
-
-		background = new Texture("fundo.png");
-
-		width = Gdx.graphics.getWidth();
-		height = Gdx.graphics.getHeight();
-		startPositionY = height/2;
+		initializeTextures();
+		initializeObject();
 	}
 
 	@Override
 	public void render () {
+		verifyState();
+		drawTextures();
+	}
+
+	private void drawTextures(){
 		batch.begin();
 
-		if(variation > 3)
-			variation = 0;
+		batch.draw(background, 0, 0, width, height);
+		batch.draw(birds[(int)  variation], 30, startPositionY);
+		batch.draw(pipeBottom, pipeWidthPosition - 100, height/2 - pipeBottom.getHeight() - (pipesSpaceBetween/2));
+		batch.draw(pipeTop, pipeWidthPosition - 100, height/2 + (pipesSpaceBetween/2));
 
+		batch.end();
+
+	}
+
+	private void verifyState(){
 		if(Gdx.input.justTouched()){
 			gravity = -15;
 		}
@@ -48,16 +52,32 @@ public class Jogo extends ApplicationAdapter {
 		if(startPositionY > 0 || gravity < 0)
 			startPositionY = startPositionY - gravity;
 
-		batch.draw(background, 0, 0, width, height);
-		batch.draw(birds[(int)  variation], 30, startPositionY);
-
 		variation += Gdx.graphics.getDeltaTime() * 10;
+		if(variation > 3)
+			variation = 0;
 
 		gravity++;
-		moveX++;
-		moveY++;
+	}
 
-		batch.end();
+	private void initializeTextures(){
+		birds = new Texture[3];
+		birds[0] = new Texture("passaro1.png");
+		birds[1] = new Texture("passaro2.png");
+		birds[2] = new Texture("passaro3.png");
+
+		background = new Texture("fundo.png");
+		pipeTop = new Texture("cano_topo_maior.png");
+		pipeBottom = new Texture("cano_baixo_maior.png");
+	}
+
+	private void initializeObject(){
+		batch = new SpriteBatch();
+
+		width = Gdx.graphics.getWidth();
+		height = Gdx.graphics.getHeight();
+		startPositionY = height/2;
+		pipeWidthPosition = width;
+		pipesSpaceBetween = 220;
 	}
 	
 	@Override
